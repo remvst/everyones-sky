da27556d284d889773e95a0da15b432a4433e743
remvst <remvst@gmail.com>
2018-08-15 14:18:41 -0400

    Fuck whitepsace

--- src/js/graphics/assets.js
+++ src/js/graphics/assets.js
@@ -3,9 +3,9 @@ const starsPattern = createPattern(STAR_PATTERN_SIZE, STAR_PATTERN_SIZE, context
     for (let i = STAR_PATTERN_SIZE ; --i ; ) {
         context.globalAlpha = rnd(0.5, 1);
         context.fillRect(
-            rnd(0, STAR_PATTERN_SIZE), 
-            rnd(0, STAR_PATTERN_SIZE), 
-            1, 
+            rnd(0, STAR_PATTERN_SIZE),
+            rnd(0, STAR_PATTERN_SIZE),
+            1,
             1
         );
     }
