b0dcacf71aa9ad07cd2db3a209a007eefb95a959
remvst <remvst@gmail.com>
2018-08-17 16:13:44 -0400

    Early HUD

--- src/js/game.js
+++ src/js/game.js
@@ -18,6 +18,31 @@ class Game {
         INTERPOLATIONS.slice().forEach(i => i.cycle(e));
 
         U.render();
+
+        // Render HUD
+        R.font = '24pt Courier';
+        R.fillStyle = '#fff';
+
+        wrap(() => {
+            translate(50, 45);
+            scale(0.5, 0.5);
+
+            beginPath();
+            moveTo(0, -15)
+            lineTo(14, -10);
+            lineTo(10, 10);
+            lineTo(0, 18);
+            lineTo(-10, 10);
+            lineTo(-14, -10);
+            fill();
+        });
+
+        R.fillStyle = 'rgba(128,128,128,0.5)';
+        fr(100, 50 - 10, 200, 10);
+
+        R.fillStyle = '#fff';
+        fr(100, 50 - 10, 100, 10);
+        fr(300, 50 - 10, 2, 10);
     }
 
 }
