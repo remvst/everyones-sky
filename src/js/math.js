6a27c3e8c1fbbc65008b31150a63f28f5eaa4378
remvst <remvst@gmail.com>
2018-08-29 00:54:17 -0400

    Removing useless keyword

--- src/js/math.js
+++ src/js/math.js
@@ -1,40 +1,40 @@
-const limit = (a, b, c) => {
+limit = (a, b, c) => {
     if (b < a) return a;
     if (b > c) return c;
     return b;
 };
 
-const between = (a, b, c) => {
+between = (a, b, c) => {
     return b >= a && b <= c;
 };
 
-const rnd = (min, max) => {
+rnd = (min, max) => {
     return random() * (max - min) + min;
 };
 
-const pick = a => {
+pick = a => {
     return a[~~(random() * a.length)];
 };
 
-const distP = (x1, y1, x2, y2) => {
+distP = (x1, y1, x2, y2) => {
     return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 };
 
-const dist = (a, b) => {
+dist = (a, b) => {
     return distP(a.x, a.y, b.x, b.y);
 };
 
-const sign = x => {
+sign = x => {
     return x < 0 ? -1 : (x > 0 ? 1 : 0);
 };
 
-const normalize = x => {
+normalize = x => {
     while (x < -PI) x += PI * 2;
     while (x > PI) x -= PI * 2;
     return x;
 };
 
-const angleBetween = (a, b) => {
+angleBetween = (a, b) => {
     return atan2(b.y - a.y, b.x - a.x);
 };
 
