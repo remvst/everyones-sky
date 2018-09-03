c5a444034f4c77b06165a0728a7f97692d680a63
remvst <remvst@gmail.com>
2018-09-03 13:23:13 -0400

    Star things

--- src/js/world/star.js
+++ src/js/world/star.js
@@ -11,6 +11,12 @@ class Star extends Body {
         // this.systemDiscovered = false; // for reference only
         this.halo = haloAround(createCanvas(this.radius * 2, this.radius * 2, () => 0), 100, 'rgba(255,0,0,0.3)', 'rgba(255,0,0,0)');
 
+
+        this.gradient = createRadialGradient(0, 0, 0, 0, 0, this.radius);
+        this.gradient.addColorStop(0, '#fff');
+        this.gradient.addColorStop(0.3, '#ff0');
+        this.gradient.addColorStop(1, '#f80');
+
         this.particleColor = () => '#ff0';
     }
 
@@ -25,14 +31,16 @@ class Star extends Body {
 
         drawImage(this.halo, this.x - this.halo.width / 2, this.y - this.halo.height / 2);
 
-        fs('#ff0');
+        translate(this.x, this.y);
+
+        fs(this.gradient);
         beginPath();
 
         for (let i = 0 ; i < 40 ; i++) {
             const a = (i / 40) * PI * 2;
             const d = sin(Date.now() / 1000 * 2 * PI * 2 + i * rndArr[i % rndArr.length]) * this.radius * 0.02 + this.radius * 0.98;
 
-            R[i ? 'lineTo' : 'moveTo'](this.x + cos(a) * d, this.y + sin(a) * d);
+            R[i ? 'lineTo' : 'moveTo'](cos(a) * d, sin(a) * d);
         }
         fill();
     }
