20ce8f0f22ee8cdd8c1cbdc86e8ed41d35758aaf
remvst <remvst@gmail.com>
2018-09-03 12:56:21 -0400

    Maybe fix asteroids mission

--- src/js/missions/asteroids.js
+++ src/js/missions/asteroids.js
@@ -15,7 +15,7 @@ class Asteroids extends TimedMissionStep {
         const asteroids = [];
         for (let i = 0 ; i < 5 ; i++) {
             const asteroid = new Asteroid();
-            asteroid.x = this.civilization.center.x + pick([-1, 1]) * 5000;
+            asteroid.x = this.civilization.center.x + pick([-1, 1]) * 3000;
             asteroid.y = this.civilization.center.y + rnd(-2500, 2500);
             asteroid.vX = sign(this.civilization.center.x - asteroid.x) * min(abs(asteroid.vX), 100);
             asteroid.vY = sign(this.civilization.center.y - asteroid.y) * min(abs(asteroid.vY), 100);
