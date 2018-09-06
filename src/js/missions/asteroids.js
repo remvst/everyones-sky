73c17575e2ba7f7e07f222844fe85403553eacc8
remvst <remvst@gmail.com>
2018-09-05 22:13:46 -0400

    Adjusted asteroids mission

--- src/js/missions/asteroids.js
+++ src/js/missions/asteroids.js
@@ -15,10 +15,10 @@ class Asteroids extends TimedMissionStep {
         const asteroids = [];
         for (let i = 0 ; i < 5 ; i++) {
             const asteroid = new Asteroid();
-            asteroid.x = this.civilization.center.x + pick([-1, 1]) * 2000;
-            asteroid.y = this.civilization.center.y + rnd(-2500, 2500);
-            asteroid.vX = sign(this.civilization.center.x - asteroid.x) * min(abs(asteroid.vX), 100);
-            asteroid.vY = sign(this.civilization.center.y - asteroid.y) * min(abs(asteroid.vY), 100);
+            asteroid.x = U.playerShip.x + pick([-1, 1]) * CANVAS_WIDTH / 0.25;
+            asteroid.y = U.playerShip.y + rnd(-1, 1) * CANVAS_HEIGHT * 2;
+            asteroid.vX = sign(U.playerShip.x - asteroid.x) * min(abs(asteroid.vX), 100);
+            asteroid.vY = sign(U.playerShip.y - asteroid.y) * min(abs(asteroid.vY), 100);
             asteroid.preventAutomaticRemoval = true;
             U.bodies.push(asteroid);
 
