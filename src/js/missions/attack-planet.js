08c78ed15e5e3458217ed7d185094c8f5a069363
remvst <remvst@gmail.com>
2018-09-07 03:00:47 -0400

    Tiny things

--- src/js/missions/attack-planet.js
+++ src/js/missions/attack-planet.js
@@ -6,19 +6,23 @@ class AttackPlanet extends TimedMissionStep {
         this.prompt = nomangle('Destroy facilities on ') + planet.nameWithRelationship();
         this.targets = [planet];
 
+        this.destroyedStations = 0;
         this.stationsToDestroy = min(this.planet.stations.length, ~~rnd(3, 5));
     }
 
     instructions() {
-        return nomangle('Destroy ') + this.stationsToDestroy + nomangle(' facilities on ') + this.planet.name;
+        return nomangle('Facilities destroyed: ') + this.destroyedStations + '/' + this.stationsToDestroy;
     }
 
     attach() {
         super.attach();
 
         this.listen(EVENT_STATION_DESTROYED, station => {
-            if (station.planet === this.planet && !--this.stationsToDestroy) {
-                G.missionDone(true);
+            if (station.planet === this.planet) {
+                this.destroyedStations++;
+                if (this.destroyedStations >= this.stationsToDestroy) {
+                    G.missionDone(true);
+                }
             }
         });
     }
